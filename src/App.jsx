0ab722e671af28f66d933ab802d4e9bbc32b7b51
simlenodir <nodirsmailov0@gmail.com>
2022-10-18 10:21:07 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+
+  return (
+    <div className="App container">
+      <button className='btn btn-outline-primary'>bytt</button>
+    </div>
+  )
+}
+
+export default App
